67c56ffe96588abeb22ae51a37014a895fa00d40
francoisdomecq <fradomecq@gmail.com>
2022-03-11 15:42:50 +0100

    Input

--- Components/HomeScreen.tsx
+++ Components/HomeScreen.tsx
@@ -1,16 +1,15 @@
-import React, {Component} from 'react'
-import {View} from 'react-native'
+import React, { Component } from 'react'
+import { View } from 'react-native'
 import Header from './Header'
+import Input from './Input'
 
 export default class HomeScreen extends Component {
-
   render() {
     return (
       <View>
-        <Header/>
+        <Header />
+        <Input />
       </View>
-
     )
   }
 }
-
--- Components/Input.tsx
+++ Components/Input.tsx
@@ -1,13 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
 
-
-export default class Input extends Component<> {
-
+export default class Input extends Component {
   render() {
     return (
-      
-
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Saisissez une nouvelle tâche"
+        ></TextInput>
+      </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  input: {
+    height: 50,
+    width: '100%',
+    fontSize: 18,
+    justifyContent: 'center',
+    color: 'black',
+    backgroundColor: 'lightgrey',
+    paddingLeft:10,
+  },
+})
